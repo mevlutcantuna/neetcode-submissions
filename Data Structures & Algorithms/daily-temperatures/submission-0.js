4ff7d72d957d183cce150ebb7068afbe38cf7dfb
Mevlüt Can Tuna <62645675+mevlutcantuna@users.noreply.github.com>
2026-07-13 09:51:31 +0300

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const n = temperatures.length;
+        const res = new Array(n).fill(0);
+
+        for (let i = 0; i < n; i++) {
+            let count = 1;
+            let j = i + 1;
+            while (j < n) {
+                if (temperatures[j] > temperatures[i]) {
+                    break;
+                }
+                j++;
+                count++;
+            }
+            count = j === n ? 0 : count;
+            res[i] = count;
+        }
+        return res;
+    }
+}
